5e886597e684169ec691df8b4789d2ff3180ae5e
Florencia Fernandez <florencia.abril.fernandez@gmail.com>
2023-05-09 17:35:39 -0300

    primero

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Diversos cajones
+const manzanas = 1;
+const peras = 2;
+const ciruelas = 3;
+
+
+
+alert ("Elegi tu cajon de frutas");
+let pregunta= prompt("¿Desea comprar algo?")
+    console.log(pregunta);
+if (pregunta === "si") {
+        let respuesta = prompt ("Por favor seleccione uno");
+    console.log(respuesta);
+    }
+    switch (seleccion) {
+    case "1":
+        manzanas ();
+        break; 
+    case "2":
+        peras ();
+        break;
+    case "3":
+        ciruelas ();
+        break; 
+    }
+
+function negacion (gracias) {
+    return gracias + "por visitarnos";
+    let negacion = ("gracias")
+    console.log(negacion);
+}
+
+
+//AGREGAR UNA FUNCION
+
+
+
+
